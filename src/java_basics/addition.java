fbff26d1e25705f5bb732ab6df18a3bcef30e3c5
Arpitha711 <marpitha56@gmail.com>
2025-01-08 15:14:54 +0530

    New file added

---
+++ src/java_basics/addition.java
@@ -0,0 +1,9 @@
+package java_basics;
+
+public class addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b=13;
+        System.out.println("Sum is" + a+b);
+    }
+}
