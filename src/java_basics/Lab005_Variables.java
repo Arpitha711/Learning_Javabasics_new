270801721625b3c1e72ff9eb2d54cb3491a31c7e
Arpitha711 <marpitha56@gmail.com>
2025-01-11 11:10:06 +0530

    New file added

---
+++ src/java_basics/Lab005_Variables.java
@@ -0,0 +1,20 @@
+package java_basics;
+
+public class Lab005_Variables {
+    public static void main(String[] args) {
+
+    }
+
+    public void main(String args) {
+
+    }
+//The main method in Java is a "critical component that serves as the entry point" for any standalone Java application.
+//there can be multiple main functions but only with this "`public static void main(String[]" JVM recognise/.`
+//Can we have multiple main method?
+//Yes can have, only one will work which is "`public static void main(String[]"`
+//Can we run a program withour a main method?
+//1.Java < 7 - Yes ( if yes
+//2.8 to 21 JDK - it was not possible
+//3.23 -> there is way.
+
+}
---
+++ src/java_excersice02/Lab006_variable.java
@@ -0,0 +1,17 @@
+package java_excersice02;
+
+public class Lab006_variable {
+    public static void main(String[] args) {
+        int a = 10;
+      //datatype = int, short, byte, float, long
+      //variable = a (identifier)
+      //variable value = 10 (literal)
+//      Data types
+//      1.Primitive - boolean and numeric
+//      2.Non priitive - class, enum, interface
+        int age = 26;
+        age=27;
+        System.out.println(age+1);
+
+    }
+}
