b4ece50ca4074511bd632a87ed3a9842d259147d
Arpitha711 <marpitha56@gmail.com>
2025-01-12 18:51:01 +0530

    New file added

--- src/java_excersice02/Lab013_Constants3.java
+++ src/java_excersice02/Lab013_Constants3.java
@@ -6,5 +6,6 @@ public class Lab013_Constants3 {
         //Can declare multiple datatype of singe variable in a single line
         int d=15;
         // not prossible int d=19;
+        //All keywords are in small letters
     }
 }
---
+++ src/java_excersice02/Lab014_Literals.java
@@ -0,0 +1,7 @@
+package java_excersice02;
+
+public class Lab014_Literals {
+    public static void main(String[] args) {
+
+    }
+}
